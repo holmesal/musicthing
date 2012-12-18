5076c0656469c4cb6875f6451c93ddd026b79dd8
pnw <patrick@levr.com>
2012-12-18 18:26:18 -0500

    totes just hacked the soundcloud sdk to make it work. 1337.

---
+++ artist_signup.py
@@ -0,0 +1,35 @@
+import webapp2
+import utils
+import sc_creds
+import logging
+import jinja2
+import os
+import utils
+import soundcloud
+jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+
+class ConnectSCHandler(utils.BaseHandler):
+	def get(self):
+		
+		template_values = {
+		}
+		
+		template = jinja_environment.get_template('templates/connect_sc.html')
+		self.response.out.write(template.render(template_values))
+	def post(self):
+		client = soundcloud.Client(
+								client_id = sc_creds.client_id,
+								client_secret = sc_creds.client_secret,
+								redirect_uri = '/signup/connect'
+								)
+		self.set_plaintext()
+		self.say(client.authorize_url())
+class ConnectAccountHandler(utils.BaseHandler):
+	def get(self):
+		pass
+app = webapp2.WSGIApplication([
+							('/signup',ConnectSCHandler),
+							('/signup/connect',ConnectAccountHandler)
+							
+							])
--- utils.py
+++ utils.py
@@ -1,4 +1,11 @@
 import webapp2
 
 class BaseHandler(webapp2.RequestHandler):
-	pass
+	def set_plaintext(self):
+		self.response.headers['Content-Type'] = 'text/plain'
+	def say(self,stuff=''):
+		'''
+		For debugging when I am too lazy to type
+		'''
+		self.response.out.write('\n')
+		self.response.out.write(stuff)
