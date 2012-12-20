982209d27bb7fc1a65e7a7feecb124829ead584d
Alonso Holmes <holmesal@bu.edu>
2012-12-20 18:57:42 -0500

    just catching up

---
+++ signup.py
@@ -0,0 +1,21 @@
+import webapp2
+import logging
+import jinja2
+import os
+import utils
+
+class SignupHandler(utils.BaseHandler):
+	def get(self):
+	
+		template_values = {
+			
+		}
+		
+		jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+		template = jinja_environment.get_template('templates/signup.html')
+		self.response.out.write(template.render(template_values))
+
+
+app = webapp2.WSGIApplication([('/signup',SignupHandler)])
+
+
