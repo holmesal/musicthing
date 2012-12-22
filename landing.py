e87b0700941a402a4ce190163033c1f7e08b71f7
Alonso Holmes <holmesal@bu.edu>
2012-12-21 23:25:23 -0600

    stuff

--- landing.py
+++ landing.py
@@ -9,27 +9,41 @@ import webapp2
 
 class LandingHandler(handlers.BaseHandler):
 	def get(self):
-		# fetch keys
-		artist_keys = models.Artist.query().fetch(None,keys_only=True)
-		try:
-			# get a random sample
-			random_artists = random.sample(artist_keys,50)
-		except ValueError:
-			# list is not big enough, return all of them
-			random_artists = artist_keys
-		# shuffle list
-		random.shuffle(random_artists)
-		artists = ndb.get_multi(random_artists)
+# 		# fetch keys
+# 		artist_keys = models.Artist.query().fetch(None,keys_only=True)
+# 		try:
+# 			# get a random sample
+# 			random_artists = random.sample(artist_keys,50)
+# 		except ValueError:
+# 			# list is not big enough, return all of them
+# 			random_artists = artist_keys
+# 		# shuffle list
+# 		random.shuffle(random_artists)
+# 		artists = ndb.get_multi(random_artists)
+# 		
+# 		# package the suckers
+# 		template_values = {
+# 						'artists' : artists
+# 		}
+
 		
-		# package the suckers
+		
+		jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+		template = jinja_environment.get_template('templates/hype.html')
+		self.response.out.write(template.render())
+
+	def post(self):
+		
+		
+		
 		template_values = {
-						'artists' : artists
+			"state"	:	"signedup"
 		}
 		
 		jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 		template = jinja_environment.get_template('templates/hype.html')
 		self.response.out.write(template.render(template_values))
-
+	
 
 app = webapp2.WSGIApplication([('/',LandingHandler)])
 
