8097786b312548581e1898e67f10c3d56c309bbe
pnw <patrick@levr.com>
2012-12-31 22:41:14 -0500

    removed sender from feedback

--- feedback.py
+++ feedback.py
@@ -8,23 +8,19 @@ class FeedbackHandler(handlers.BaseHandler):
 #		pass
 	def post(self):
 		body = self.request.get('body')
-		sender = self.request.get('from')
 		body = body.encode('ascii','ingore')
-		sender = sender.encode('ascii','ignore')
 		
 		try:
 			message = mail.AdminEmailMessage(
 											sender = 'patrick@levr.com',
 											subject = 'New Merchant',
 											)
-			message.body = 'From: '+sender+'\n\n\n'
 			message.body += body
 			message.check_initialized()
 			message.send()
 			
 		except:
 			logging.critical('Feedback Message could not send')
-			logging.critical(sender)
 			logging.critical(body)
 app = webapp2.WSGIApplication([
 							('/feedback',FeedbackHandler)
