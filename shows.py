94983d101b84859057f19ce294fa7861f68044a0
Alonso Holmes <holmesal@bu.edu>
2013-01-16 19:44:23 -0500

    added test handler for bandpage

--- shows.py
+++ shows.py
@@ -14,17 +14,27 @@ class ShowHandler(handlers.BaseHandler):
 		jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 		template = jinja_environment.get_template('templates/bandshowsignup.html')
 		self.response.out.write(template.render())
+		
+class TestHandler(handlers.BaseHandler):
+	def get(self):
+	
+		jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+		template = jinja_environment.get_template('templates/bandpage.html')
+		self.response.out.write(template.render())
 
 
 class PlayCheckHandler(handlers.BaseHandler):
 	def get(self):
 		
+		#respond with true if logged in, false if not
+		
 		response = {
-			"loggedin"	:	True
+			"loggedin"	:	False
 		}
 		self.response.out.write(json.dumps(response))
 
 app = webapp2.WSGIApplication([	('/shows',ShowHandler),
+								('/shows/test',TestHandler),
 								('/shows/playcheck',PlayCheckHandler)])
 
 
