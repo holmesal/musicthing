b050ee710da0074f6d860a0b6d9aa354f2418e18
Alonso Holmes <holmesal@bu.edu>
2013-01-16 22:07:08 -0500

    merge

--- shows.py
+++ shows.py
@@ -17,10 +17,29 @@ class ShowHandler(handlers.BaseHandler):
 		
 class TestHandler(handlers.BaseHandler):
 	def get(self):
-	
+		
+		going = ["going person 1","going person 2","going person 3"]
+		
+		artist = {
+			"track_id"		:	53097530,
+			"artist_name"	:	"The Band"
+		}
+		
+		template_values = {
+			"tickets_total"	:	40,
+			"tickets_remaining"	:	40,
+			"tickets_sold"		:	0,
+			"place_string"		:	"2nd",
+			"going"			:	going,
+			"artist"		:	artist,
+			"contestant_id"	:	"1x3rp",
+			"status"		:	"won",
+			"tickets_available"	:	False
+		}
+		
 		jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 		template = jinja_environment.get_template('templates/bandpage.html')
-		self.response.out.write(template.render())
+		self.response.out.write(template.render(template_values))
 
 
 class PlayCheckHandler(handlers.BaseHandler):
